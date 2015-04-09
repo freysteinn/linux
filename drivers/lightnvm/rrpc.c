0ce5b8d54d22a172ad9fee7294473dfc59de8468
Javier González <javier@javigon.com>
2015-04-09 10:13:23 +0200

    lightnvm: fix style

--- drivers/lightnvm/rrpc.c
+++ drivers/lightnvm/rrpc.c
@@ -488,7 +488,7 @@ static struct nvm_addr *rrpc_map_page(struct rrpc *rrpc, sector_t laddr,
 	rlun = __rrpc_get_lun_rr(rrpc, is_gc);
 	lun = rlun->parent;
 
-	if ( !is_gc && lun->nr_free_blocks < 2 * rrpc->nr_luns ) {
+	if (!is_gc && lun->nr_free_blocks < rrpc->nr_luns * 2) {
 		return NULL;
 	}
 
